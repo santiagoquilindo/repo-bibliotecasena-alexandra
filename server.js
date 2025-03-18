f3842697a45974a8a0fd954761fbbdb9c37d6381
santiagoquilindo <santiagoquilindo32@gmail.com>
2025-03-18 16:49:48 -0500

    cambios hoy

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-dotenv.config();        
+dotenv.config();
 app.use(express.json());
 
 app.use("/api", require("./router/prestamoRouter"));
-app.use("/api", require("./router/usuarioRouter"));  
-app.use("/api", require("./router/libroRouter"));  
+app.use("/api", require("./router/usuarioRouter"));
+app.use("/api", require("./router/libroRouter"));
 
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
-});           
+});
+
